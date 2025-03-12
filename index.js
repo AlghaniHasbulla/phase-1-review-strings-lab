c96a08b2a37132dfe44a6a4cb0c0d9f962bf9f5c
Alghani Hasbulla <george.munene1@student.moringaschool.com>
2025-03-12 13:39:24 +0300

    String lab 10 passing

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Hasbulla"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()+currentUser.toUpperCase+"!"
+const shortGreeting="Welcome, "+currentUser.slice(0,1)+"!";
